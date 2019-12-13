3161c35d4532b36b5f5b2a5f55250c1810870606
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2019-12-13 15:11:29 +0200

    Do not reverse

--- SwiftUIAnimationSamples/AnimationDuration/Repeating_RepeatCount.swift
+++ SwiftUIAnimationSamples/AnimationDuration/Repeating_RepeatCount.swift
@@ -26,7 +26,7 @@ struct Repeating_RepeatCount: View {
                     .stroke(Color.green, lineWidth: 4)
                     .scaleEffect(start ? 2 : 1)
                     .opacity(start ? 0 : 1)
-            ).animation(Animation.easeOut(duration: 0.6).repeatCount(3))
+            ).animation(Animation.easeOut(duration: 0.6).repeatCount(3, autoreverses: false))
             Spacer()
         }.font(.title)
     }
