89b23e01a7d9516baae05f6e0b6b435fa48e3c0c
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2019-12-13 12:59:47 +0200

    Add the Hide/Show logic

--- SwiftUIAnimationSamples/AnimationTriggers/Exercise/Popup.swift
+++ SwiftUIAnimationSamples/AnimationTriggers/Exercise/Popup.swift
@@ -9,6 +9,7 @@
 import SwiftUI
 
 struct Popup: View {
+    @State private var showPopup = true // Controls if the popup shows or not
     var body: some View {
         ZStack {
             VStack(spacing: 20) {
@@ -18,32 +19,43 @@ struct Popup: View {
                 Spacer()
                 Button("Shoe Popup") {
                     // Show popup
+                    self.showPopup = true
                 }
             }.font(.title)
+            // Blur the background when popup is showing
+                .blur(radius: showPopup ? 2 : 0)
             
             // The popup
-            ZStack {
-                // Background transparent color
-                Color.black
-                    .opacity(0.4)
-                    .edgesIgnoringSafeArea(.all)
-                
-                VStack(spacing: 20) {
-                    Text("Drag popup off the screen")
-                        .padding()
-                        .frame(maxWidth: .infinity)
-                        .background(Color.yellow)
-                    Spacer()
-                    Text("You can also tap outside of the popup ro close")
-                    Spacer()
-                    Button("Or Tap This Button ti Close") {
-                        // Dismiss group
-                    }.padding()
-                }.frame(height: 300)
-                .background(Color.white)
-                .cornerRadius(20)
-                .shadow(radius: 20)
-                    .padding(.horizontal, 25)
+            // Put the Popup inside an if
+            if showPopup {
+                ZStack {
+                    // Background transparent color
+                    Color.black
+                        .opacity(0.4)
+                        .edgesIgnoringSafeArea(.all)
+                        // Dismiss popup
+                        .onTapGesture {
+                            self.showPopup = false
+                    }
+                    
+                    VStack(spacing: 20) {
+                        Text("Drag popup off the screen")
+                            .padding()
+                            .frame(maxWidth: .infinity)
+                            .background(Color.yellow)
+                        Spacer()
+                        Text("You can also tap outside of the popup ro close")
+                        Spacer()
+                        Button("Or Tap This Button ti Close") {
+                            // Dismiss group
+                            self.showPopup = false
+                        }.padding(.bottom)
+                    }.frame(height: 300)
+                    .background(Color.white)
+                    .cornerRadius(20)
+                    .shadow(radius: 20)
+                        .padding(.horizontal, 25)
+                }
             }
         }
     }
