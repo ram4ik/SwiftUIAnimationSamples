09429d832b765f1a4105f2191867da55a6af16d8
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2019-12-13 13:47:32 +0200

    add animations

--- SwiftUIAnimationSamples/AnimationTriggers/Exercise/Popup.swift
+++ SwiftUIAnimationSamples/AnimationTriggers/Exercise/Popup.swift
@@ -23,8 +23,9 @@ struct Popup: View {
                     self.showPopup = true
                 }
             }.font(.title)
-            // Blur the background when popup is showing
+                // Blur the background when popup is showing
                 .blur(radius: showPopup ? 2 : 0)
+                .animation(.easeOut) // Animate the blur and unblur
             
             // The popup
             // Put the Popup inside an if
@@ -52,27 +53,27 @@ struct Popup: View {
                             self.showPopup = false
                         }.padding(.bottom)
                     }.frame(height: 300)
-                    .background(Color.white)
-                    .cornerRadius(20)
-                    .shadow(radius: 20)
-                    .padding(.horizontal, 25)
-                    .offset(popupOffset) // Offset (move) the popup when dragged
-                    .gesture(
-                        DragGesture(minimumDistance: 100) // They have to drag it over 100 points
-                            .updating($popupOffset, body: { (value, popupOffset, transaction) in
-                                // Assign distance traveled (translation) to popupOffset (Gesture State variable that is bound:
-                                
-                                popupOffset = value.translation
-                            })
-                            .onEnded({ (value) in
-                                // If they dragged the popup over 100 points then just close the popup
-                                self.showPopup = false
-                            })
-                    
+                        .background(Color.white)
+                        .cornerRadius(20)
+                        .shadow(radius: 20)
+                        .padding(.horizontal, 25)
+                        .offset(popupOffset) // Offset (move) the popup when dragged
+                        .gesture(
+                            DragGesture(minimumDistance: 100) // They have to drag it over 100 points
+                                .updating($popupOffset, body: { (value, popupOffset, transaction) in
+                                    // Assign distance traveled (translation) to popupOffset (Gesture State variable that is bound:
+                                    
+                                    popupOffset = value.translation
+                                })
+                                .onEnded({ (value) in
+                                    // If they dragged the popup over 100 points then just close the popup
+                                    self.showPopup = false
+                                })
+                            
                     )
-                }
+                }.animation(.default) // Animate the popup
             }
-        }
+        }.animation(.easeIn) // This animation will be used to hide/show the popup
     }
 }
 
